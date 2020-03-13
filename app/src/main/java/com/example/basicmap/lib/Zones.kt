b4fe6472cf292a2fcfa18dcefa5e76232262c796
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-03-13 14:14:15 +0100

    add Zone stub
    We need to get different zones onto the map. And we probably need to
    get data from several sources.
    
    So it makes sense for us to have some sort of abstraction to deal with
    this in a consistent manner.

---
+++ app/src/main/java/com/example/basicmap/lib/Zones.kt
@@ -0,0 +1,10 @@
+package com.example.basicmap.lib
+/*
+    Utility for querying various zone data, like no-fly zones etc.
+
+    Could for instance have an method that takes a rectangle (ie. four LatLang's) and returns
+    a collection of all zones (polygons) inside the rectangle.
+ */
+
+class Zones {
+}
