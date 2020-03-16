61a5d4d94de9b612b287b7b01e5a55c98577cd90
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-03-16 17:00:50 +0100

    map: make polygon tracer
    listen to clicks and use them to trace a polygon

--- app/src/main/java/com/example/basicmap/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/basicmap/ui/home/HomeFragment.kt
@@ -4,24 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.basicmap.R
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var mMap: GoogleMap
-    private lateinit var marker: Marker
+    private var marker: Marker? = null
+    private val positions = mutableListOf<LatLng>()
+    private var polygon: Polygon? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,19 +36,23 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListene
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        marker = mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
         mMap.setOnMapClickListener(this)
     }
 
     override fun onMapClick(p0: LatLng?) {
         if (p0 == null)
             return
-        marker.remove()
+
+        marker?.remove()
         marker = mMap.addMarker(MarkerOptions().position(p0))
+        positions.add(p0)
+
+        // This is a bit ugly, and should really only be in a constructor of some sorts,
+        // but creating an empty polygon doesn't seem to work.
+        // So we delay it until we have at least on point
+        if (polygon == null)
+            polygon = mMap.addPolygon(PolygonOptions().addAll(positions))
+
+        polygon?.points = positions
     }
 }
