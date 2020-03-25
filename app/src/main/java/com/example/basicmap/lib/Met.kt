eb61e917fefb9bdf77e3efdf387b8847165d74d3
Johancb <johancb@uio.no>
2020-03-25 16:42:53 +0100

    home popup: populate with weather data

--- app/src/main/java/com/example/basicmap/lib/Met.kt
+++ app/src/main/java/com/example/basicmap/lib/Met.kt
@@ -62,8 +62,8 @@ class Met {
         val response = Fuel.get(fullUrl).awaitString()
         //Log.d("Url", fullUrl)
         val weather = gson.fromJson(response, Kall::class.java)
-        Log.d("temp verdi", weather.product.time[0].location.temperature?.value) //test som henter nåværende temp
-        Log.d("regn verdi", weather.product.time[1].location.precipitation?.value)//test som henter nåværende regn
+        //Log.d("temp verdi", weather.product.time[0].location.temperature?.value) //test som henter nåværende temp
+        //Log.d("regn verdi", weather.product.time[1].location.precipitation?.value)//test som henter nåværende regn
         return weather
     }
 }
