176317651c060efb56bc306ddff3da10f89520f9
Olav Moen <olavmoen98@gmail.com>
2020-03-27 12:18:56 +0100

    Slett drone fra lista

--- app/src/main/java/com/example/basicmap/ui/drones/ListAdapter.kt
+++ app/src/main/java/com/example/basicmap/ui/drones/ListAdapter.kt
@@ -1,16 +1,22 @@
 package com.example.basicmap.ui.drones
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.basicmap.R.layout.drone_kort
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.drone_kort.view.*
 
-class ListAdapter(val context: Context, val droneList: MutableList<Drone>) : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ListAdapter(val context: Context, val droneList: MutableList<Drone>) : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         fun setData(drone: Drone?, pos: Int) {
             itemView.navn.setText(drone?.navn)
             itemView.vindStyrke.setText("Maks vindstyrke: " + drone?.maksVindStyrke.toString())
@@ -20,7 +26,34 @@ class ListAdapter(val context: Context, val droneList: MutableList<Drone>) : Rec
             else {
                 itemView.vanntett.setText("Ikke vanntett")
             }
+            itemView.deleteKnapp.setOnClickListener {
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Slett Drone")
+                builder.setMessage("Er du sikker på at du vil slette \n" + drone?.navn + " ?")
+                builder.setPositiveButton("Ja") { dialog, which ->
+                    slettDrone(pos)
+                }
+                builder.setNegativeButton("Nei") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.show()
+            }
         }
+        fun slettDrone(pos: Int) {
+            droneList.removeAt(pos)
+            notifyDataSetChanged()
+            saveData()
+            Log.v("test", "drone slettet")
+        }
+        private fun saveData() {
+            val sharedPref: SharedPreferences = context.getSharedPreferences("sharedPref", AppCompatActivity.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPref.edit()
+            val gson = GsonBuilder().create()
+            val json = gson.toJson(droneList)
+            editor.putString("droneList", json)
+            editor.commit()
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
