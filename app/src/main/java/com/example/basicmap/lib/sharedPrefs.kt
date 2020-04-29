2ba64795f96a3f36959df7cc81446d1dd0d592ca
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-04-29 17:19:38 +0200

    refactor save/loadPlaces into lib

--- app/src/main/java/com/example/basicmap/lib/sharedPrefs.kt
+++ app/src/main/java/com/example/basicmap/lib/sharedPrefs.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import com.example.basicmap.ui.drones.Drone
+import com.example.basicmap.ui.places.Place
 import com.google.gson.GsonBuilder
 
 fun saveDrones(context: Context, drones: List<Drone>?): String? {
@@ -24,4 +25,22 @@ fun loadDrones(context: Context): MutableList<Drone>? {
     val json = sharedPref.getString("droneList", null)
     val drones = gson.fromJson(json, Array<Drone>::class.java) ?: return null
     return drones.toMutableList()
+}
+
+fun loadPlaces(context: Context): MutableList<Place>? {
+    val sharedPrefPlaces: SharedPreferences = context.getSharedPreferences("sharedPrefPlaces", AppCompatActivity.MODE_PRIVATE)
+    val gson = GsonBuilder().create()
+    val json = sharedPrefPlaces.getString("placesList", null)
+    val places = gson.fromJson(json, Array<Place>::class.java) ?: return null
+    return places.toMutableList()
+}
+
+fun savePlaces(context: Context, places: List<Place>): String? {
+    val sharedPref: SharedPreferences = context.getSharedPreferences("sharedPrefPlaces", AppCompatActivity.MODE_PRIVATE)
+    val editor: SharedPreferences.Editor = sharedPref.edit()
+    val gson = GsonBuilder().create()
+    val json = gson.toJson(places)
+    editor.putString("placesList", json)
+    editor.apply()
+    return json
 }
--- app/src/main/java/com/example/basicmap/ui/places/PlacesFragment.kt
+++ app/src/main/java/com/example/basicmap/ui/places/PlacesFragment.kt
@@ -16,6 +16,8 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.basicmap.R
+import com.example.basicmap.lib.loadPlaces
+import com.example.basicmap.lib.savePlaces
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.fragment_places.*
 import kotlinx.android.synthetic.main.fragment_places.view.*
@@ -36,7 +38,7 @@ class PlacesFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_places, container, false)
 
-        loadData()
+        placesViewModel.getPlaces().value = loadPlaces(requireContext())
 
         placesViewModel.getPlaces().observe(viewLifecycleOwner, Observer<MutableList<Place>> {
             viewManager = LinearLayoutManager(activity)
@@ -52,28 +54,10 @@ class PlacesFragment : Fragment() {
             else {
                 recycleViewTekstPlaces.visibility = INVISIBLE
             }
-            saveData()
+            savePlaces(requireContext(), it)
         })
 
         return root
     }
 
-    private fun loadData() {
-        val sharedPrefPlaces: SharedPreferences = requireActivity().getSharedPreferences("sharedPrefPlaces", AppCompatActivity.MODE_PRIVATE)
-        val gson = GsonBuilder().create()
-        val json = sharedPrefPlaces.getString("placesList", null)
-        val places = gson.fromJson(json, Array<Place>::class.java) ?: return
-        Log.d("placeslist", json)
-        placesViewModel.getPlaces().value = places.toMutableList()
-    }
-
-    private fun saveData() {
-        Log.d("saveData", "")
-        val sharedPref: SharedPreferences = requireActivity().getSharedPreferences("sharedPrefPlaces", AppCompatActivity.MODE_PRIVATE)
-        val editor: SharedPreferences.Editor = sharedPref.edit()
-        val gson = GsonBuilder().create()
-        val json = gson.toJson(placesViewModel.getPlaces().value)
-        editor.putString("placesList", json)
-        editor.apply()
-    }
 }
