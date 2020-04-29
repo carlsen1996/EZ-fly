38c05a95ff1b713924d9af1243ef4122ba3703b8
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-04-29 17:24:33 +0200

    sharedPrefs: refactor keys into variables

--- app/src/main/java/com/example/basicmap/lib/sharedPrefs.kt
+++ app/src/main/java/com/example/basicmap/lib/sharedPrefs.kt
@@ -7,40 +7,46 @@ import com.example.basicmap.ui.drones.Drone
 import com.example.basicmap.ui.places.Place
 import com.google.gson.GsonBuilder
 
+private val SHARED_PREF = "sharedPrefs"
+private val DRONES = "saved_drones"
+private val PLACES = "saved_places"
+
 fun saveDrones(context: Context, drones: List<Drone>?): String? {
     val sharedPref: SharedPreferences =
-        context.getSharedPreferences("sharedPref", AppCompatActivity.MODE_PRIVATE)
+        context.getSharedPreferences(SHARED_PREF, AppCompatActivity.MODE_PRIVATE)
     val editor: SharedPreferences.Editor = sharedPref.edit()
     val gson = GsonBuilder().create()
     val json = gson.toJson(drones)
-    editor.putString("droneList", json)
+    editor.putString(DRONES, json)
     editor.apply()
     return json
 }
 
 fun loadDrones(context: Context): MutableList<Drone>? {
     val sharedPref: SharedPreferences =
-        context.getSharedPreferences("sharedPref", AppCompatActivity.MODE_PRIVATE)
+        context.getSharedPreferences(SHARED_PREF, AppCompatActivity.MODE_PRIVATE)
     val gson = GsonBuilder().create()
-    val json = sharedPref.getString("droneList", null)
+    val json = sharedPref.getString(DRONES, null)
     val drones = gson.fromJson(json, Array<Drone>::class.java) ?: return null
     return drones.toMutableList()
 }
 
 fun loadPlaces(context: Context): MutableList<Place>? {
-    val sharedPrefPlaces: SharedPreferences = context.getSharedPreferences("sharedPrefPlaces", AppCompatActivity.MODE_PRIVATE)
+    val sharedPrefPlaces: SharedPreferences =
+        context.getSharedPreferences(SHARED_PREF, AppCompatActivity.MODE_PRIVATE)
     val gson = GsonBuilder().create()
-    val json = sharedPrefPlaces.getString("placesList", null)
+    val json = sharedPrefPlaces.getString(PLACES, null)
     val places = gson.fromJson(json, Array<Place>::class.java) ?: return null
     return places.toMutableList()
 }
 
 fun savePlaces(context: Context, places: List<Place>): String? {
-    val sharedPref: SharedPreferences = context.getSharedPreferences("sharedPrefPlaces", AppCompatActivity.MODE_PRIVATE)
+    val sharedPref: SharedPreferences =
+        context.getSharedPreferences(SHARED_PREF, AppCompatActivity.MODE_PRIVATE)
     val editor: SharedPreferences.Editor = sharedPref.edit()
     val gson = GsonBuilder().create()
     val json = gson.toJson(places)
-    editor.putString("placesList", json)
+    editor.putString(PLACES, json)
     editor.apply()
     return json
 }
