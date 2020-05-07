f168556a361b9621ac91a1cfa91d2f7d9eeb0c5c
ahadel <ahadel@student.matnat.uio.no>
2020-05-07 14:47:28 +0200

    initial wind stuffz

--- app/src/main/java/com/example/basicmap/lib/PopuplateWeather.kt
+++ app/src/main/java/com/example/basicmap/lib/PopuplateWeather.kt
@@ -1,9 +1,12 @@
 package com.example.basicmap.lib
 
 import android.content.Context
+import android.text.SpannableStringBuilder
 import android.util.Log
 import android.view.View
+import androidx.core.text.bold
 import com.example.basicmap.R
+import kotlinx.android.synthetic.main.popup.*
 import kotlinx.android.synthetic.main.weather.view.*
 import java.time.DayOfWeek
 import java.time.LocalDate
@@ -57,11 +60,18 @@ fun populateWeather(context: Context, container: View, weather: Met.Kall) {
             if (datetime.dayOfWeek == now.dayOfWeek || datetime.hour == 12) {
                 Log.d("now", datetime.toString())
                 val tempNow = data.data.instant.details.air_temperature?.toDouble()?.roundToInt().toString()
-                container.precipitationView.text = "NEDBØR\n${data.data.next_6_hours?.details?.probability_of_precipitation ?: ""}%" //regn eller nedbør riktig her?
-                container.visibilityView.text = "TÅKE\n${data.data.instant.details.fog_area_fraction}%"
-                container.kpindexView.text = "KP\n3"
-                container.tempValue.text = "${tempNow}°C"
+                var precipProb = weather.properties.timeseries[0].data.instant.details.probability_of_precipitation
+                var wind = weather.properties.timeseries[0].data.instant.details.wind_speed
+                var windDirection = weather.properties.timeseries[0].data.instant.details.wind_from_direction
+                var compassDeg = windDirection?.toDouble()?.let { degToCompass(it) }
+                var windGust = weather.properties.timeseries[0].data.instant.details.wind_speed_of_gust
 
+                container.tempValue.text = "${tempNow}°C"
+                container.precipValue.text = "${precipProb}"
+                container.windValue.text = "${wind}"
+                container.windDesc.text = "m/s ${compassDeg}"
+                var customGustString = SpannableStringBuilder().append("Vindkast: ").bold {append("$windGust")}.append(" m/s")
+                container.windGustValue.text = customGustString
                 val weatherIconName = data.data.next_6_hours?.summary?.symbol_code
                 val id = context.resources.getIdentifier(weatherIconName, "mipmap", context.packageName)
                 container.weatherImageView.setImageResource(id)
@@ -82,4 +92,10 @@ fun populateWeather(context: Context, container: View, weather: Met.Kall) {
         null -> container.dayBar.clearCheck()
     }
 
+}
+
+private fun degToCompass(a : Double) : String {
+    var b =((a/22.5)+.5).toInt()
+    var c = arrayOf("N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW")
+    return c[(b % 16)]
 }
