281b59732a3b62120ce31bf8646c1484bc0cb3b0
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-05-11 16:32:45 +0200

    cleanup

--- app/src/main/java/com/example/basicmap/lib/PopuplateWeather.kt
+++ app/src/main/java/com/example/basicmap/lib/PopuplateWeather.kt
@@ -23,68 +23,6 @@ import java.time.format.TextStyle
 import java.util.*
 import kotlin.math.roundToInt
 
-
-private fun populateWeather(context: Context, container: View, livePlace: LivePlace) {
-    val weather = livePlace.weather.value
-    if (weather == null)
-        return
-
-    val utc = DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.of("UTC"))
-    val timeseries = weather.properties.timeseries
-
-    val days = mapOf(
-        DayOfWeek.MONDAY to mutableListOf<Met.Numb>(),
-        DayOfWeek.TUESDAY to mutableListOf<Met.Numb>(),
-        DayOfWeek.WEDNESDAY to mutableListOf<Met.Numb>(),
-        DayOfWeek.THURSDAY to mutableListOf<Met.Numb>(),
-        DayOfWeek.FRIDAY to mutableListOf<Met.Numb>(),
-        DayOfWeek.SATURDAY to mutableListOf<Met.Numb>(),
-        DayOfWeek.SUNDAY to mutableListOf<Met.Numb>()
-    )
-    val now = LocalDate.now()
-    for (data in timeseries) {
-        val time = data.time
-        val date = LocalDate.from(utc.parse(time))
-
-        if (date.isAfter(now.plusDays(6)))
-            break
-
-        days[date.dayOfWeek]?.add(data)
-    }
-
-    val day = livePlace.day.value!!
-    for (data in days[day.dayOfWeek]!!) {
-        val time = data.time
-        val datetime = LocalDateTime.from(utc.parse(time))
-
-        if (datetime.dayOfWeek == now.dayOfWeek || datetime.hour == 12) {
-            Log.d("now", datetime.toString())
-            val tempNow =
-                data.data.instant.details.air_temperature?.toDouble()?.roundToInt().toString()
-            val precipProb = data.data.next_6_hours?.details?.probability_of_precipitation
-            val wind = data.data.instant.details.wind_speed
-            val windDirection = data.data.instant.details.wind_from_direction
-            val compassDeg = windDirection?.toDouble()?.let { degToCompass(it) }
-            val windGust = data.data.instant.details.wind_speed_of_gust
-
-            container.tempValue.text = "${tempNow}°C"
-            container.precipValue.text = "${precipProb}"
-            container.windValue.text = "${wind}"
-            container.windDesc.text = "m/s ${compassDeg}"
-            val customGustString =
-                SpannableStringBuilder().append("Vindkast: ").bold { append("$windGust") }
-                    .append(" m/s")
-            container.windGustValue.text = customGustString
-            val weatherIconName = data.data.next_6_hours?.summary?.symbol_code
-            val id = context.resources.getIdentifier(weatherIconName, "mipmap", context.packageName)
-            container.weatherImageView.setImageResource(id)
-            break
-        }
-    }
-
-
-}
-
 fun degToCompass(a : Double) : String {
     var b =((a/22.5)+.5).toInt()
     var c = arrayOf("N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW")
@@ -124,7 +62,6 @@ fun setupWeatherElement(
     }
 
     livePlace.day.observe(lifecycleOwner, Observer {
-        populateWeather(context, container, livePlace)
     })
 
     container.hourScrollView.layoutManager =
