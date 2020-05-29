036e0e07b95ac1a0f7460e92c9796de82022ced8
Povland <43108424+Povland@users.noreply.github.com>
2020-05-29 13:55:31 +0200

    FAQ regelverk etc

--- app/src/main/java/com/example/basicmap/ui/info/InfoFragment.kt
+++ app/src/main/java/com/example/basicmap/ui/info/InfoFragment.kt
@@ -16,15 +16,22 @@ import kotlinx.android.synthetic.main.fragment_info.view.*
 
 class InfoFragment : Fragment() {
     val questions = listOf(
+        "Les dette før du flyr",
         "Kan jeg fly dronen min i minusgrader?",
         "Hva hvis det regner/snør?",
         "Hvor høyt kan jeg fly?",
         "Kan jeg fly om kvelden?",
         "Hvor finner jeg lover om droneflyging i Norge?",
+        "Obs Nytt regelverk",
         "Produsert av: ",
         "En spesiell takk til: "
     )
     val answers = listOf(
+        listOf("Det er sterkt anbefalt at alle brukere leser seg opp på dronelovene i Norge.\n\n" +
+                "Det er veldig mange regler som handler om hvor man kan fly og ikke fly. " +
+                "Appen viser hovedsakelig No-Fly sonene rundt lufthavnene, men det er mange lokale regler man må passe på. \n" +
+                "Noen ting å passe på er Natur-reservater, festivaler, sportsarrangementer, skoler, sykehus og noen offentlige områder \n\n" +
+                "Lykke til!"),
         listOf("Det er ikke anbefalt, men mulig. Batteriet mister raskt " +
                 "kapasitet i kulden. Pass på at dronen din er ladet" +
                 ". Et tips er å bruke håndvarmere for å " +
@@ -34,10 +41,12 @@ class InfoFragment : Fragment() {
         listOf("Luftfartstilsynet sin regel er ikke høyere enn 120m over bakken."),
         listOf("Hovedregelen er at du alltid må kunne se dronen når den flys. Dersom " +
                 "det er mørkt og du har dårlig sikt, skal du ikke fly."),
-        listOf("Du kan finne hovedreglene her : \n" +
-                "https://luftfartstilsynet.no/droner/hobbydrone/dronelek/\n" +
-                "Fullt regelverk her : \n"+
+        listOf("Du kan finne hovedreglene her : \n\n" +
+                "https://luftfartstilsynet.no/droner/hobbydrone/dronelek/\n\n" +
+                "Fullt regelverk her : \n\n"+
                 "https://lovdata.no/dokument/SF/forskrift/2015-11-30-1404\n"),
+        listOf("Fra og med 1 Januar 2021 så vil det EU regelverket bli gjeldene i Norge."+
+                "Vi anbefaler brukere å lese seg opp på de nye reglene før det nye regelverket trer i kraft."),
         listOf("Al Hareth Adel\n"+
                 "Johannes Carlsen\n"+
                 "Olav Moen\n"+
